9f56fb1ada325bb0656419144780b8f1483bc172
Aaron Chen <aaron.chen11219@gmail.com>
2020-06-19 15:03:00 -0400

    Two Separate Graphs for two different indexes

--- parsing.py
+++ parsing.py
@@ -1,17 +1,15 @@
 import csv 
 import matplotlib.pyplot as plt
+import plotly.express as px
+import pandas as pd
 import numpy as np
 
 url = 'https://raw.githubusercontent.com/hongwilliam/Random-Walk-Down-Wall-Street/master/fredgraph.csv'
-dates = pd.read_csv(url, usecols = ['DATE'])
-usd_yen = pd.read_csv(url, usecols = ['DEXJPUS'])
-dow = pd.read_csv(url, usecols = ['DJIA'])
-
-# dates
-plt.plot(dates, usd_yen)
-plt.plot(dates, dow)
-
-plt.show()
+csv_file = pd.read_csv(url)
+fig = px.line(csv_file, x = 'DATE', y = 'DEXJPUS')
+fig.show()
+figone = px.line(csv_file, x = 'DATE', y = 'DJIA')
+figone.show()
 
 
 
