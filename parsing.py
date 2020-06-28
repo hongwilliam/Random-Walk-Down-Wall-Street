d471828430ced7675ba5fdbdbebaac748249983b
Aaron Chen <53270187+AaronBinChen@users.noreply.github.com>
2020-06-28 00:17:31 -0400

    Labels
    Updated the graphs with proper x-axis, y-axis and title labels

--- parsing.py
+++ parsing.py
@@ -6,10 +6,7 @@ import numpy as np
 
 url = 'https://raw.githubusercontent.com/hongwilliam/Random-Walk-Down-Wall-Street/master/fredgraph.csv'
 csv_file = pd.read_csv(url)
-fig = px.line(csv_file, x = 'DATE', y = 'DEXJPUS')
+fig = px.line(csv_file, x = 'DATE', y = 'DEXJPUS', labels = {'DEXJPUS':'Japanese Yen for one USD'}, title = "Japan/U.S Foreign Exchange Rate 10 Year Period")
 fig.show()
-figone = px.line(csv_file, x = 'DATE', y = 'DJIA')
+figone = px.line(csv_file, x = 'DATE', y = 'DJIA',labels = {'DJIA':'Dow Jones Industrial Average in USD'}, title = "Dow Jones Price 10 Year Period")
 figone.show()
-
-
-
