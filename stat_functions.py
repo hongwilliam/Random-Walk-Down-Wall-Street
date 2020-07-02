fa97d94c471d750e558c586daa53c2a40a284fd7
hongwilliam <willhong20@gmail.com>
2020-07-01 23:11:54 -0400

    CLEANED UP STAT FUNCTIONS, ADDED WORKING COVARIANCE CALCULATOR

--- stat_functions.py
+++ stat_functions.py
@@ -1,49 +1,49 @@
 import cgitb
 cgitb.enable()
-import math
-import numpy
-import scipy
+import numpy as np
+import pandas as pd
 
-#basic statistics exercise
+#note to developers: go on terminal in the directory this file is in
+#type python stat_functions.py to excecute the file
 
-def compute_mean(list):
-    sum = 0
-    i = 0
-    while i < len(list):
-        sum += list[i]
-        i += 1
-    #print(sum / len(list))
-    return sum / len(list)
+#TESTING BASIC STATISTICAL FUNCTIONS
 
-def compute_variance(list):
-    mean = compute_mean(list)
-    sum_squared_deviations = 0
-    squared_deviation = 0
-    n = len(list)
-    i = 0
-    while i < len(list):
-        squared_deviation = (list[i] - mean) ** 2
-        sum_squared_deviations += squared_deviation
-        i += 1
-    variance = sum_squared_deviations / (n-1)
-    #print (variance)
-    return variance
 
-def compute_standard_deviation(list):
-    #this is for sample standard deviation, not population
-    variance = compute_variance(list)
-    print (math.sqrt(variance))
-    return math.sqrt(variance)
+#PART 1: testing the mean function
+list_1 = np.array([1, 2, 3, 4, 5])
+print(np.mean(list_1)) #should return 3
 
-def compute_z_score(x, mu, sigma):
-    #mu = mean, sigma = standard deviation
-    #represents how many SDs that x is away from the mean
-    #print ((x-mu)/sigma)
-    return (x-mu)/sigma
 
-list_1 = [1.23, 2.12, 3.34, 4.5]
+#PART 2: testing mean on 2 dimensional arrays
+list_2 = np.array([ [1, 2, 3], [4, 5, 6] ]) #list of lists
+print(np.mean(list_2)) #should return 3.5
+print(np.mean(list_2[0])) #should return mean of first list, which is 2
 
-list_2 = [2.56, 2.89, 3.76, 3.95]
-covariance = numpy.stack((list_1, list_2), axis = 0)
+#should return mean of 1 and 4, 2 and 5, 3 and 6... which is [2.5 3.5 4.5]
+print(np.mean(list_2, axis = 0))
 
-print(numpy.cov(covariance))
+#should return mean of 1,2,3 and 4,5,6... which is [2. 5.]
+print(np.mean(list_2, axis = 1))
+
+
+#PART 3: testing the standard deviation function
+list_3 = np.array([1, 2, 3, 4])
+print(np.std(list_3)) #should return 1.118 ish
+
+
+#PART 4: testing the covariance function
+#covariance > 0 is positively correlated, 0 is uncorrelated, < 0 is negatively
+
+#perfect negative correlation
+list_4 = [0, 1, 2]
+list_5 = [2, 1, 0]
+list_4 = pd.Series(list_4)
+list_5 = pd.Series(list_5)
+print(list_4.cov(list_5)) #should return -1
+
+#perfect positive correlation
+list_6 = [0, 1, 2]
+list_7 = [1, 2, 3]
+list_6 = pd.Series(list_6)
+list_7 = pd.Series(list_7)
+print(list_6.cov(list_7)) #should return 1
