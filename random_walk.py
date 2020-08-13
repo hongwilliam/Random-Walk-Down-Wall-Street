bce8bcc77fa9a609c145cd1ed0e24cd4e3f2adf1
hongwilliam <willhong20@gmail.com>
2020-08-13 16:12:05 -0400

    Added exponential moving average and fixed exception handling for dates

--- random_walk.py
+++ random_walk.py
@@ -115,8 +115,7 @@ def compute_correlation_companies(company_1, company_2, start_year, start_month,
         print("Day out of range")
         sys.exit()
 
-    # Error: start date later than end date
-    if ( (a < d) or (a == d and ( (b < e) or (c < f) ) ) ):
+    if ( (a > d) or (a == d and ( (b > e) or ( (b == e and c > f) ) ) ) ):
         print ("Start date must be earlier than end date")
         sys.exit()
 
@@ -183,7 +182,7 @@ def compute_volatility(company, start_year, start_month, start_day, end_year, en
         sys.exit()
 
     # Error: start date later than end date
-    if ( (a < d) or (a == d and ( (b < e) or (c < f) ) ) ):
+    if ( (a > d) or (a == d and ( (b > e) or ( (b == e and c > f) ) ) ) ):
         print ("Start date must be earlier than end date")
         sys.exit()
 
@@ -245,8 +244,7 @@ def compute_average_daily_price_change(company, start_year, start_month, start_d
         print("Day out of range")
         sys.exit()
 
-    # Error: start date later than end date
-    if ( (a < d) or (a == d and ( (b < e) or (c < f) ) ) ):
+    if ( (a > d) or (a == d and ( (b > e) or ( (b == e and c > f) ) ) ) ):
         print ("Start date must be earlier than end date")
         sys.exit()
 
@@ -289,7 +287,7 @@ def compute_stochastic_oscillator(company, year, month, day):
         sys.exit()
 
     # Error: day out of range for leap year
-    if ((b == 2) and (c > 29) and (a % 4 == 0):
+    if ((b == 2) and (c > 29) and (a % 4 == 0)):
         print("Day out of range")
         sys.exit()
 
@@ -330,6 +328,70 @@ def compute_stochastic_oscillator(company, year, month, day):
     x = str(answer) + "%"
     print(x)
     return answer
+
+def compute_exponential_moving_average(company, start_year, start_month, start_day, end_year, end_month, end_day):
+        a = int(start_year)
+        b = int(start_month)
+        c = int(start_day)
+        d = int(end_year)
+        e = int(end_month)
+        f = int(end_day)
+
+        # Error: same start and end date
+        if ((a == d) and (b == e) and (c == f)):
+            print("The start and end dates must be different")
+            sys.exit()
+
+        # Error: the months exist out of range
+        if ((b < 1 or b > 12) or (e < 1 or e > 12)):
+            print("Month out of range")
+            sys.exit()
+
+        # Error: days are out of range for non-leap years and regular months
+        if ((c < 1) or (f < 1) or (c > 31) or (f > 31) or ( (b == 2 or e == 2) and (c > 28 or f > 28) and (a % 4 != 0 or d % 4 != 0 ) ) ):
+            print("Day out of range")
+            sys.exit()
+
+        # Error: day out of range for leap year
+        if ((b == 2 or e == 2) and (c > 29 or f > 29) and (a % 4 == 0 or d % 4 == 0)):
+            print("Day out of range")
+            sys.exit()
+
+        # Error: handle months with 31 days
+        if ((b == 1 or b == 3 or b == 5 or b == 7 or b == 8 or b == 10 or b == 12 or
+             e == 1 or e == 3 or e == 5 or e == 7 or e == 8 or e == 10 or e == 12 ) and (c > 31)):
+            print("Day out of range")
+            sys.exit()
+
+        # Error: handle months with 30 days
+        if ((b == 4 or b == 6 or b == 9 or b == 11 or e == 4 or e == 6 or e == 9 or e == 11 ) and (c > 30)):
+            print("Day out of range")
+            sys.exit()
+
+        # Error: start date later than end date
+        if ( (a > d) or (a == d and ( (b > e) or ( (b == e and c > f) ) ) ) ):
+            print ("Start date must be earlier than end date")
+            sys.exit()
+
+        temp = pdr.get_data_yahoo(symbols = company, start =  datetime(a, b, c), end = datetime(d, e, f))
+        comp_list = []
+        x = 0
+        while (x < len(temp)):
+            entry = temp['Close'][x]
+            entry = round(entry, 2)
+            comp_list.append(entry)
+            x += 1
+
+        comp_list = pd.Series(comp_list)
+        comp_list = comp_list.ewm(span = x, adjust=False).mean()
+        answer = round(comp_list[1305], 2)
+
+        return answer
+
+
+
+print(compute_exponential_moving_average('TSLA', 2015, 6, 7, 2020, 8, 12))
+
 
 
 #PART 4: entering command line input to get analysis
