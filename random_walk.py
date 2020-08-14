92e7030dee7953be0822aec9cc8b52592b194543
Aaron Chen <53270187+AaronBinChen@users.noreply.github.com>
2020-08-13 22:31:23 -0400

    Plots of Price and Volume next to each other
    Refer to the following video and guide to understand how one can make decisions of stock trading based on stock volume
    
    Link 1: https://school.stockcharts.com/doku.php?id=technical_indicators:on_balance_volume_obv#:~:text=On%20Balance%20Volume%20(OBV)%20is,and%20the%20OBV%20line%20falls.
    
    Link 2: https://www.youtube.com/watch?v=pVaA-FKkF8c

--- random_walk.py
+++ random_walk.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import scipy.stats
 import pandas_datareader as pdr
+import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 import sys
 
@@ -388,10 +389,77 @@ def compute_exponential_moving_average(company, start_year, start_month, start_d
         answer = round(comp_list[x], 2)
 
         return answer
+# print(compute_exponential_moving_average('TSLA', 2015, 6, 7, 2020, 8, 12))
 
+def stock_obv_visual(company, start_year, start_month, start_day, end_year, end_month, end_day):
+    a = int(start_year)
+    b = int(start_month)
+    c = int(start_day)
+    d = int(end_year)
+    e = int(end_month)
+    f = int(end_day)
 
+    # Error: same start and end date
+    if ((a == d) and (b == e) and (c == f)):
+        print("The start and end dates must be different")
+        sys.exit()
 
-print(compute_exponential_moving_average('TSLA', 2015, 6, 7, 2020, 8, 12))
+    # Error: the months exist out of range
+    if ((b < 1 or b > 12) or (e < 1 or e > 12)):
+        print("Month out of range")
+        sys.exit()
+
+    # Error: days are out of range for non-leap years and regular months
+    if ((c < 1) or (f < 1) or (c > 31) or (f > 31) or ( (b == 2 or e == 2) and (c > 28 or f > 28) and (a % 4 != 0 or d % 4 != 0 ) ) ):
+        print("Day out of range")
+        sys.exit()
+
+    # Error: day out of range for leap year
+    if ((b == 2 or e == 2) and (c > 29 or f > 29) and (a % 4 == 0 or d % 4 == 0)):
+        print("Day out of range")
+        sys.exit()
+
+    # Error: handle months with 31 days
+    if ((b == 1 or b == 3 or b == 5 or b == 7 or b == 8 or b == 10 or b == 12 or
+         e == 1 or e == 3 or e == 5 or e == 7 or e == 8 or e == 10 or e == 12 ) and (c > 31)):
+        print("Day out of range")
+        sys.exit()
+
+    # Error: handle months with 30 days
+    if ((b == 4 or b == 6 or b == 9 or b == 11 or e == 4 or e == 6 or e == 9 or e == 11 ) and (c > 30)):
+        print("Day out of range")
+        sys.exit()
+
+    # Error: start date later than end date
+    if ( (a > d) or (a == d and ( (b > e) or ( (b == e and c > f) ) ) ) ):
+        print ("Start date must be earlier than end date")
+        sys.exit()
+    
+    # Obtaining data from the Yahoo Finance Website
+    # style.use('ggplot')
+    temp = pdr.get_data_yahoo(company, start =  datetime(a, b, c), end = datetime(d, e, f))
+    # print(temp)
+    temp.reset_index(inplace = True)
+    temp.set_index("Date", inplace = True)
+
+    # Plotting the Close Price
+    plt.figure(figsize = (50, 10))
+    plt.plot(temp['Close'])
+    plt.title('Price History Over Time')
+    plt.ylabel('Share Price in USD')
+    plt.xlabel('Date')
+    plt.show()
+
+    # Plotting the Volume
+    plt.figure(figsize = (50, 10))
+    plt.plot(temp['Volume']/1000000)
+    plt.title('Trade Volumes Over Time')
+    plt.ylabel('Trade Volumes in Millions')
+    plt.xlabel('Date')
+    plt.show()
+    # return temp.tail(20)
+
+print(stock_obv_visual('TSLA', 2019, 6, 7, 2020, 8, 12))
 
 
 
